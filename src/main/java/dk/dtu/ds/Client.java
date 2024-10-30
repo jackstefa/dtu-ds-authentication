d35cca95dd5e8c359cbd039bc7ee43353d441c6e
jackstef <s242952@dtu.dk>
2024-10-30 19:07:19 +0100

    Init

---
+++ src/main/java/dk/dtu/ds/Client.java
@@ -0,0 +1,16 @@
+package dk.dtu.ds;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class Client {
+
+    public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
+
+        HelloService service = (HelloService) Naming.lookup("rmi://localhost:5099/hello");
+        System.out.println("--- " + service.echo("Hey Server!"));
+    }
+
+}
---
+++ src/main/java/dk/dtu/ds/HelloServant.java
@@ -0,0 +1,16 @@
+package dk.dtu.ds;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloServant extends UnicastRemoteObject implements HelloService {
+
+    protected HelloServant() throws RemoteException {
+        super();
+    }
+
+
+    public String echo(String input) throws RemoteException {
+        return "From server: " + input;
+    }
+}
