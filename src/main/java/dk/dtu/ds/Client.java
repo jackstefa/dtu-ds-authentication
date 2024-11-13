3946d59122d32400bd2dd097a8d0c786a5c32b87
Federico Marra <fecrom@gmail.com>
2024-11-13 16:45:14 +0100

    Fix main in client

--- src/main/java/dk/dtu/ds/Client.java
+++ src/main/java/dk/dtu/ds/Client.java
@@ -4,9 +4,9 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
 
 public class Client {
     private static AuthenticationService authService;
@@ -15,17 +15,34 @@ public class Client {
 
     private static String token;
 
-    public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
-        authService = (AuthenticationService) Naming.lookup(Config.AUTHENTICATION_ENDPOINT);
-        token = getToken();
-        executeUserRequests();
+    public static void main(String[] args) {
+        try {
+            authService = (AuthenticationService) Naming.lookup(Config.AUTH_ENDPOINT);
+            token = getToken();
+            if (token != null){
+                System.out.println("Token: " + token);
+                executeUserRequests();
+            } else {
+                System.out.println("Login failed");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Execution failed1: " + e.getMessage());
+        } catch (RemoteException e) {
+            System.out.println("Lookup failed2: " + e.getMessage());
+        } catch (NotBoundException e) {
+            System.out.println("Lookup failed3: " + e.getMessage());
+        } catch (MalformedURLException e) {
+            System.out.println("Lookup failed4: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 
     public static String getToken() {
         while (true) {
-            System.out.print("Enter username :");
+            System.out.print("Enter username: ");
             String username = scanner.nextLine();
-            System.out.print("Enter password :");
+            System.out.print("Enter password: ");
             String password = scanner.nextLine();
             try {
                 return authService.login(username, password);
@@ -35,7 +52,7 @@ public class Client {
         }
     }
 
-    public static void executeUserRequests() throws MalformedURLException, NotBoundException, RemoteException {
+    public static void executeUserRequests() throws MalformedURLException, NotBoundException, InterruptedException, RemoteException {
         while (true) {
             System.out.println("Enter command: ");
             String command = scanner.nextLine();
@@ -49,6 +66,11 @@ public class Client {
                 } catch (RemoteException e) {
                     System.out.println("Print failed" + e.getMessage());
                 }
+            }
+            try {
+                sleep(1000);    // sleep for 1 second, busy waiting
+            } catch (InterruptedException e) {
+                System.out.println("Sleep failed" + e.getMessage());
             }
         }
     }
