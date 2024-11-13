eb36346a1cece7e336d064c4975e315934d82981
Federico Marra <fecrom@gmail.com>
2024-11-13 16:11:48 +0100

    Refactor PrintServant

--- src/main/java/dk/dtu/ds/PrintServant.java
+++ src/main/java/dk/dtu/ds/PrintServant.java
@@ -18,72 +18,81 @@ public class PrintServant extends UnicastRemoteObject implements PrintService {
 
     @Override
     public void print(String token, String filename, String printer) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         System.out.println("Printing " + filename + " on " + printer);
     }
 
     @Override
     public List<String> queue(String token, String printer) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         return List.of("Job 1", "Job 2", "Job 3");
     }
 
     @Override
     public void topQueue(String token, String printer, int job) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         System.out.println("Moving job " + job + " to the top of the queue");
     }
 
     @Override
     public void start(String token) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         System.out.println("Starting print service");
     }
 
     @Override
     public void stop(String token) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         System.out.println("Stopping print service");
     }
 
     @Override
     public void restart(String token) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         System.out.println("Restarting print service");
     }
 
     @Override
     public String status(String token, String printer) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         return "Printer " + printer + " is ready";
     }
 
     @Override
     public String readConfig(String token, String parameter) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         return "Value of " + parameter;
     }
 
     @Override
     public void setConfig(String token, String parameter, String value) throws RemoteException {
-        if (!authService.isTokenValid(token))
+        if (!authService.isTokenValid(token)) {
             throw new RemoteException("Invalid token");
+        }
 
         System.out.println("Setting " + parameter + " to " + value);
     }
